c5b0cd38fc09ffdefff4085ba4476b020f28847f
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2024-11-29 13:29:05 -0800

    looping lab done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards( array, event,) {
+    const messages = []
+  for(let i = 0; i < array.length; i++) {
+    console.log(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return messages
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+function countDown(number) {
+
+    while(number >= 0){
+        console.log(number--)
+    }
+}
